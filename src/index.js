1d4c05a8c8d34d878c20d979c7ed5f03c4caf236
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-05-15 19:04:13 +0530

    API ENDPOINTS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("server started");
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
+
+const DB_url = `${config.mongoose.url}`
+mongoose
+.connect(DB_url)
+.then(()=>{console.log("its connected to MongoDb Atlas")})
+.catch((err)=>{console.log(err)});
+
+app.listen(config.port,()=>{
+    console.log("connected to port",config.port)
+})
